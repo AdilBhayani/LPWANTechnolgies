fc880c9fce8e6881c9484778c7e2c2130d006e54
Adil Bhayani <abha808@aucklanduni.ac.nz>
2018-09-13 17:32:42 +1200

    Finalising last graph

--- js/newMain.js
+++ js/newMain.js
@@ -13,6 +13,9 @@ function callbackFunction() {
 	MoistureData.addColumn('number', 'Buried node');
 	MoistureData.addColumn('number', 'Surface node');
 	var TemperatureData = new google.visualization.DataTable();
+	TemperatureData.addColumn('string', 'Time');
+	TemperatureData.addColumn('number', 'Buried node');
+	TemperatureData.addColumn('number', 'Surface node');
 	function drawChart() {
 		$.ajax({
 			url: '../CombinedRSSI.json',
@@ -89,6 +92,38 @@ function callbackFunction() {
 			  MoistureData.addRows(chartData);
 			}
 		  });
+		  
+		  $.ajax({
+			url: '../CombinedTemperature.json',
+			type: 'get',
+			success: function (chartData) {
+			  var index = chartData.length - 1;
+			  var set = false;
+			  while (! set && index >= 0){
+				  if (chartData[index][1] != null){
+					  set = true;
+					  var moisture = chartData[index][1].toString() + 'degrees at ' + chartData[index][0].toString();
+					  if (document.getElementById("soil_temperature_div").innerHTML = moisture){
+						document.getElementById("soil_temperature_div").innerHTML = moisture;
+					  }
+				  }
+				  index = index - 1;
+			  }
+			  if (chartData.length > 50) {
+			      var first = [];
+                  first.push(chartData[0]);
+                  //console.log(first);
+                  chartData = chartData.slice(Math.max(chartData.length - 50, 1));
+                  chartData = first.concat(chartData);
+                  //console.log(chartData);
+			  }
+			  TemperatureData = new google.visualization.DataTable();
+			  TemperatureData.addColumn('string', 'Time');
+			  TemperatureData.addColumn('number', 'Buried node');
+			  TemperatureData.addColumn('number', 'Surface node');
+			  TemperatureData.addRows(chartData);
+			}
+		  });
 		
 		var RSSIChart = new google.visualization.LineChart(document.getElementById('signal_strength_div'));
 		var options = {
@@ -147,28 +182,32 @@ function callbackFunction() {
 		};
 		MoistureChart.draw(MoistureData,options);
 		
-		
-		var data = new google.visualization.DataTable();
-		data.addColumn('string', 'Topping');
-		data.addColumn('number', 'Slices');
-		data.addRows([
-		  ['Mushrooms', 3],
-		  ['Onions', 1],
-		  ['Olives', 1],
-		  ['Zucchini', 1],
-		  ['Pepperoni', 2]
-		]);
-		/*var piechart_options = {title:'Pie Chart: How Much Pizza I Ate Last Night',
-			width:400,
-			height:300};
-		var piechart = new google.visualization.PieChart(document.getElementById('moisture_div'));
-		piechart.draw(data, piechart_options);*/
-
-		var barchart_options = {title:'Barchart: How Much Pizza I Ate Last Night',
-			   width:400,
-			   height:300,
-			   legend: 'none'};
-		var barchart = new google.visualization.BarChart(document.getElementById('temperature_div'));
-		barchart.draw(data, barchart_options);
+		var TemperatureChart = new google.visualization.LineChart(document.getElementById('temperature_div'));
+		var options = {
+			interpolateNulls: true,
+			pointSize: 8,
+			chartArea: {
+				left: 50,
+				width: '100%',
+				top: 35
+			},
+			legend: {
+				position: 'bottom'
+			},
+			width: '100%',
+			height: 400,
+			hAxis: {
+				title: "Time",
+			},
+			vAxis: {
+				title: "Temperature in degrees",
+				viewWindow: {
+					min: 10,
+					max: 25
+				},
+				ticks: [10, 12.5, 15, 17.5, 20, 22.5, 25]				
+			}
+		};
+		TemperatureChart.draw(TemperatureData,options);
 	}
 }
