4e41cedd3e06fc1b9cc4ff1f57ee2f98abff2279
Adil Bhayani <abha808@aucklanduni.ac.nz>
2018-09-13 15:18:50 +1200

    Modifying php to create only combined data

--- dataIn.php
+++ dataIn.php
@@ -29,7 +29,12 @@
        fclose($fl);
      }
     if (strcmp($_station,"3597") == 0) {
-        if ( $f2 = fopen(($_id. 'RSSI.json'),'a')) {
+		
+		$dataStr = '';
+		for($i=0;$i<strlen($_data);$i+=2) $dataStr .= chr(hexdec(substr($_data,$i,2)));
+		$valuesArray = explode(',', $dataStr);
+		
+        /*if ( $f2 = fopen(($_id. 'RSSI.json'),'a')) {
             fwrite($f2, "[\"". $_dt . "\",". $_rssi. "]]");
             fclose($f2);
         }
@@ -41,7 +46,7 @@
             fclose($f2);
         }
         $f2 = $_id. 'SNR.json';
-        file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
+        file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));*/
 		
 		if (strcmp($_id,"3E3370") == 0 or strcmp($_id,"3E3652") == 0){
 			if ( $f2 = fopen(('CombinedRSSI.json'),'a')) {
@@ -54,25 +59,45 @@
 			}
 			$f2 = 'CombinedRSSI.json';
 			file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
+			
+			if ( $f2 = fopen(('CombinedMoisture.json'),'a')) {
+				if (strcmp($_id,"3E3370") == 0){
+					fwrite($f2, "[\"". $_dt . "\",". $valuesArray[0]. ",". "null". "]]");
+				}else{
+					fwrite($f2, "[\"". $_dt . "\",". "null". ",". $valuesArray[0]. "]]");
+				}
+				fclose($f2);
+			}
+			$f2 = 'CombinedMoisture.json';
+			file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
+			
+			if ( $f2 = fopen(('CombinedTemperature.json'),'a')) {
+				if (strcmp($_id,"3E3370") == 0){
+					fwrite($f2, "[\"". $_dt . "\",". $valuesArray[1]. ",". "null". "]]");
+				}else{
+					fwrite($f2, "[\"". $_dt . "\",". "null". ",". $valuesArray[1]. "]]");
+				}
+				fclose($f2);
+			}
+			$f2 = 'CombinedTemperature.json';
+			file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
 		}
 		
-		$dataStr = '';
-		for($i=0;$i<strlen($_data);$i+=2) $dataStr .= chr(hexdec(substr($_data,$i,2)));
-		$valuesArray = explode(',', $dataStr);
-		
-		if ( $f2 = fopen(($_id. 'Moisture.json'),'a')) {
+		/*if ( $f2 = fopen(($_id. 'Moisture.json'),'a')) {
             fwrite($f2, "[\"". $_dt . "\",". $valuesArray[0]. "]]");
             fclose($f2);
         }
         $f2 = $_id. 'Moisture.json';
         file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
 		
+		
+		
 		if ( $f2 = fopen(($_id. 'Temperature.json'),'a')) {
             fwrite($f2, "[\"". $_dt . "\",". $valuesArray[1]. "]]");
             fclose($f2);
         }
         $f2 = $_id. 'Temperature.json';
-        file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));
+        file_put_contents($f2,str_replace(']][','],[',file_get_contents($f2)));*/
     }
   ?>
   </body>
